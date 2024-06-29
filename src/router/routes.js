8a9df9ce9872460f84134b85fdcee81ff5bf7d84
Karthikeyan110803 <karthikeyan11803@gmail.com>
2024-06-29 12:00:20 +0530

    corrections commited

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,60 +1,10 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    component: () => import('pages/Crosspuzzle.vue'),
+   
   },
 
-  {
-    path: '/puzzles',
-    
-    children: [
-      { path: '', component: () => import('pages/Puzzle.vue') }
-    ]
-  },
-  {
-    path: '/cross',
-    
-    children: [
-      { path: '', component: () => import('pages/Crosspuzzle.vue') }
-    ]
-  },
-  {
-    path: '/puzz',
-    
-    children: [
-      { path: '', component: () => import('pages/Puzz.vue') }
-    ]
-  },
-  {
-    path: '/new',
-    
-    children: [
-      { path: '', component: () => import('pages/New.vue') }
-    ]
-  },
-  {
-    path: '/quiz',
-    
-    children: [
-      { path: '', component: () => import('pages/Quiz.vue') }
-    ]
-  },
-  {
-    path: '/word',
-    
-    children: [
-      { path: '', component: () => import('pages/Word.vue') }
-    ]
-  },
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
 ]
 
 export default routes
